fbd78dc5cccc84632599d89ee6e0d77a22790eb4
Gregory Smith <57313630+GPRSBirkbeck@users.noreply.github.com>
2020-08-16 20:08:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/feedme/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.example.feedme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//abstract class as it is never "really" used
+public abstract class BaseActivity extends AppCompatActivity {
+}
